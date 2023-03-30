a9f918369bc7dc0c7b5430e7b9ce1e6b241fe974
Dustin Carlino <dabreegster@gmail.com>
2023-03-30 16:15:50 +0100

    Start a test framework

--- rust/tests/src/lib.rs
+++ rust/tests/src/lib.rs
@@ -1,7 +1,19 @@
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use overline::{overline, Input, Output};
 
-    #[test]
-    fn test_all() {}
+    include!(concat!(env!("OUT_DIR"), "/tests.rs"));
+
+    fn test(input_path: &str, output_path: &str) {
+        let input: Vec<Input> = geojson::de::deserialize_feature_collection_str_to_vec(
+            &std::fs::read_to_string(input_path).unwrap(),
+        )
+        .unwrap();
+        let actual_output = overline(&input);
+        let expected_output: Vec<Output> = geojson::de::deserialize_feature_collection_str_to_vec(
+            &std::fs::read_to_string(output_path).unwrap(),
+        )
+        .unwrap();
+        assert_eq!(actual_output, expected_output);
+    }
 }
